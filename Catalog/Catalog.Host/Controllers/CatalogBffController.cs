05874365894754e09f930b69f01e725f38d31cbc
Aleksey Veselovskiy <ballarnes@gmail.com>
2022-01-16 22:34:38 +0200

    Completed homework

--- Catalog/Catalog.Host/Controllers/CatalogBffController.cs
+++ Catalog/Catalog.Host/Controllers/CatalogBffController.cs
@@ -1,3 +1,4 @@
+using Catalog.Host.Data.Entities;
 using Catalog.Host.Models.Dtos;
 using Catalog.Host.Models.Requests;
 using Catalog.Host.Models.Response;
@@ -22,9 +23,91 @@ public class CatalogBffController : ControllerBase
 
     [HttpPost]
     [ProducesResponseType(typeof(PaginatedItemsResponse<CatalogItemDto>), (int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
     public async Task<IActionResult> Items(PaginatedItemsRequest request)
     {
         var result = await _catalogService.GetCatalogItemsAsync(request.PageSize, request.PageIndex);
+
+        if (result == null)
+        {
+            return BadRequest();
+        }
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(CatalogItemDto), (int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    public async Task<IActionResult> GetById(GetByIdRequest request)
+    {
+        var result = await _catalogService.GetByIdAsync(request.Id);
+
+        if (result == null)
+        {
+            return BadRequest();
+        }
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(PaginatedItemsResponse<CatalogItemDto>), (int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    public async Task<IActionResult> GetByBrand(GetByBrandRequest request)
+    {
+        var result = await _catalogService.GetByBrandAsync(request.PageSize, request.PageIndex, request.Brand!);
+
+        if (result == null)
+        {
+            return BadRequest();
+        }
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(PaginatedItemsResponse<CatalogItemDto>), (int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    public async Task<IActionResult> GetByType(GetByTypeRequest request)
+    {
+        var result = await _catalogService.GetByTypeAsync(request.PageSize, request.PageIndex, request.Type!);
+
+        if (result == null)
+        {
+            return BadRequest();
+        }
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(PaginatedItemsResponse<CatalogItemDto>), (int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    public async Task<IActionResult> GetBrands(PaginatedItemsRequest request)
+    {
+        var result = await _catalogService.GetBrandsAsync(request.PageSize, request.PageIndex);
+
+        if (result == null)
+        {
+            return BadRequest();
+        }
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(PaginatedItemsResponse<CatalogItemDto>), (int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    public async Task<IActionResult> GetTypes(PaginatedItemsRequest request)
+    {
+        var result = await _catalogService.GetTypesAsync(request.PageSize, request.PageIndex);
+
+        if (result == null)
+        {
+            return BadRequest();
+        }
+
         return Ok(result);
     }
 }
